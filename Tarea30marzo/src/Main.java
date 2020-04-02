1d61d572c414be5f86ea8a2933adbf00106ea058
hpabnerdom <abnerdomhp@hotmail.com>
2020-04-02 12:21:13 -0600

    Update Main.java

--- Tarea30marzo/src/Main.java
+++ Tarea30marzo/src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         System.out.println("Nombre: " + "Denis Joel Cifuentes Cano");
         System.out.println("Direccion: "+ "Zona 1 Chiantla");
         System.out.println("Numero cel: " + "45883960");
+        
+            System.out.println("Nombre: " + "Abner Obed Domínguez Rodríguez");
+            System.out.println("Direccion: " + "Colonia El Oasis, Zona 8, Huehuetenango");
+            System.out.println("Numero Tel: " + "42252923");//Editado por Abner Dominguez
+
     }
     
 }
