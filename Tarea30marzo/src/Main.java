6c8aeb7f7d6b5f8782287c68969c7b5ad18f1955
Usuario <Usuario@192.168.1.71>
2020-04-02 11:56:12 -0600

    Esto es el programa base hecho por mi: Denis Joel Cifuentes Cano

---
+++ Tarea30marzo/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Nombre: " + "Denis Joel Cifuentes Cano");
+        System.out.println("Direccion: "+ "Zona 1 Chiantla");
+        System.out.println("Numero cel: " + "45883960");
+    }
+    
+}
